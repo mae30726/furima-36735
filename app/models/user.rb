eec3c3957ef062fc6b7398919ba6f96a640d0dd9
mae30726 <mae30726@gmail.com>
2021-09-17 12:35:43 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ApplicationRecord
          
   has_many :items
   has_many :purchases
+
+  validates :nickname, presence: true
+  validates :email
+  validates :encrypted_password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  validates :user_last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]/+\z }
+  validates :user_first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]/+\z }
+  validates :user_last_name_kana, presence: true, format: { with: /\p{katakana}//\A[ァ-ヶー－]+\z/ }
+  validates :user_first_name_kana, presence: true, format: { with: /\p{katakana}//\A[ァ-ヶー－]+\z/ }
+  validates :birthday, presence: true
+
 end
