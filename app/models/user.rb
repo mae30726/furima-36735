ca2e4cde81c24f12517a0b366fefb50353bd4a36
mae30726 <mae30726@gmail.com>
2021-09-19 06:30:41 +0900

    ユーザー新規登録/ログイン/ログアウト機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,11 @@ class User < ApplicationRecord
   has_many :purchases
 
   validates :nickname, presence: true
-  validates :email
-  validates :encrypted_password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-  validates :user_last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]/+\z }
-  validates :user_first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]/+\z }
-  validates :user_last_name_kana, presence: true, format: { with: /\p{katakana}//\A[ァ-ヶー－]+\z/ }
-  validates :user_first_name_kana, presence: true, format: { with: /\p{katakana}//\A[ァ-ヶー－]+\z/ }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  validates :user_last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :user_first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :user_last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :user_first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday, presence: true
 
 end
