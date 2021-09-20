cee5fd426be8a9134ee133a4ccf7477e4190eac6
mae30726 <mae30726@gmail.com>
2021-09-20 15:54:32 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,20 @@
 class Item < ApplicationRecord
   validates :item_name, presence: true
   validates :explanation, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :shipping_cost_responsible_id, presence: true
-  validates :shipping_from_id, presence: true
-  validates :shipping_date_id, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_cost_responsible_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_from_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_date_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :price, presence: true
 
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_cost_responsible
+  belongs_to :shipping_from
+  belongs_to :shipping_date
 end
