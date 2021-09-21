e3b0def5bf9b0686daaef65cf918df49a892ba10
mae30726 <mae30726@gmail.com>
2021-09-21 12:18:00 +0900

    商品出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,25 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
   def new
+    @item = Item.new
   end
 
+  def index
+    @items = Item.all
+  end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+        redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :item_name, :explanation, :category_id, :condition_id, :shipping_cost_responsible_id, :shipping_from_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+  end
 end
