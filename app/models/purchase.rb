5537291c418e736a0dfd242042231059513d4eaa
mae30726 <mae30726@gmail.com>
2021-10-01 12:34:11 +0900

    アソシエーションの削除

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -2,8 +2,4 @@ class Purchase < ApplicationRecord
   has_one :destination
   belongs_to :item
   belongs_to :user
-  
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :prefecture
-
 end
